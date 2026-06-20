1b9fdabee4754dc4349e143187d24000dc0b8897
reDdboy <rephose@mail.ru>
2026-06-20 20:44:31 +0300

    Исправлен блюр header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -40,7 +40,7 @@ const Header = () => {
 
     return (
         <>
-            <header className="bg-background/200 backdrop-blur-md border-b border-church-gold/40 dark:border-church-gold/60 py-2 sm:py-3 transition-colors duration-300 sticky top-0 z-50">
+            <header className="bg-background/70 backdrop-blur-md border-b border-church-gold/40 dark:border-church-gold/60 py-2 sm:py-3 transition-colors duration-300 sticky top-0 z-50">
                 <div className="max-w-7xl mx-auto px-3 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between gap-2">
                         {/* Левая часть: фото и название */}
